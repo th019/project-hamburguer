c78121c2d62f9d5b85b81c7d2bfee18c6b7855b6
th019 <ematheusdgar@gmail.com>
2024-05-21 16:23:44 -0300

    first commit project-hamburguer

---
+++ products.js
@@ -0,0 +1,8 @@
+const products = [
+{name: "X-Bacon egg", price: 35, vegan: false, src: "./assets/bacon-egg.png"},
+{name: "X-Bacon", price: 30, vegan: false, src: "./assets/xbacon.png"},
+{name: "X-Monstruoso", price: 42.5, vegan: false, src: "./assets/monstruoso.png"},
+{name: "X-Salada", price: 28, vegan: false, src: "./assets/xsalada.jpeg"},
+{name: "X-Vegano", price: 30, vegan: true, src: "./assets/xvegan.png"},
+{name: "X-Monstruoso vegano", price: 40, vegan: true, src: "./assets/monstruoso-vegan.png"},
+]
---
+++ script.js
@@ -0,0 +1,63 @@
+const list = document.querySelector("ul")
+const seeAll = document.querySelector(".seeAll-button")
+const discont = document.querySelector(".discont-button")
+const sum = document.querySelector(".sum-button")
+const filtering = document.querySelector(".filter-button")
+
+function newValue(value) {
+    return new Intl.NumberFormat('pt-br', {
+         style: 'currency',
+     currency: 'BRL' 
+
+    }).format(value)
+}
+
+const show = (productsArray) => {
+    let myLi = ''
+
+    productsArray.forEach(Poduct => {
+        // += é uma abreviaçao de myLi + myLi = ``
+        myLi += `       
+            <li>
+            <img src=${Poduct.src}>
+            <p>${Poduct.name}</p>
+            <p class="price"> ${newValue(Poduct.price)}</p>
+            </li>
+        `
+
+    })
+    list.innerHTML = myLi
+}
+
+const disconting = () => {
+
+    const newPrice = products.map(item => ({
+        /*spread operator */...item,
+        price: item.price * 0.9
+    }))
+
+    show(newPrice)
+}
+
+const sumAll = () => {
+    const total = products.reduce((acc, curr) => acc + curr.price, 0)
+    list.innerHTML = `
+     <li>
+     <p>A soma de todos os itens do menu é </p>
+     <p class="price"> ${newValue(total)}</p>
+     </li>
+     `
+
+}
+
+function filterAll() {
+    const fill = products.filter(fill => fill.vegan)
+
+    show(fill)
+
+}
+
+seeAll.addEventListener("click", () => show(products))
+discont.addEventListener("click", disconting)
+sum.addEventListener("click", sumAll)
+filtering.addEventListener("click", filterAll)
